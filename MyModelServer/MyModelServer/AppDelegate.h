423e924cbe2ef0ae6d8ad0e9f746590ed9fdd6ce
Bad <badboywww@hotmail.com>
2018-10-16 14:27:47 +0800

    ...

---
+++ MyModelServer/MyModelServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyModelServer
+//
+//  Created by Bad on 2018/10/16.
+//  Copyright © 2018 Bad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyModelServer/MyModelServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyModelServer
+//
+//  Created by Bad on 2018/10/16.
+//  Copyright © 2018 Bad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
